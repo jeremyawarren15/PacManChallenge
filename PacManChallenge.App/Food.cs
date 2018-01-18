dfd2ee5ec9d3e7f26b7cc1ecc9526c7414e46e42
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-18 14:13:33 -0500

    completed challenge

---
+++ PacManChallenge.App/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacManChallenge.App
+{
+    public enum Food
+    {
+        Dot = 10,
+        Cherry = 100,
+        Strawberry = 300,
+        Orange = 500,
+        Apple = 700,
+        Melon = 1000,
+        Galaxian = 2000,
+        Bell = 3000,
+        Key = 5000,
+        Ghost = 200
+    }
+}
